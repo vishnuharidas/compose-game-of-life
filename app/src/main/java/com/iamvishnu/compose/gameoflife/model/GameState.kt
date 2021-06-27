df6d25ef64667237c75d21b3c8f5638f817958ee
Vishnu Haridas <hvishnu999@gmail.com>
2021-06-28 01:24:31 +0530

    Add a state field to mark if reached still-life.

--- app/src/main/java/com/iamvishnu/compose/gameoflife/model/GameState.kt
+++ app/src/main/java/com/iamvishnu/compose/gameoflife/model/GameState.kt
@@ -3,9 +3,21 @@ package com.iamvishnu.compose.gameoflife.model
 data class GameState(
     val isPaused: Boolean = false,
     val generation: Int = 0,
+    val isStillLife: Boolean = false,
     val cells: Matrix<Boolean>, // Row major list.
 ){
     val population: Int by lazy {
         cells.count { it }
+    }
+
+    val status: String get() {
+        return when {
+            isStillLife -> "Reached Still Life."
+            !isPaused -> "Progressing..."
+            isPaused && generation == 0 && population == 0 -> "Ready. Tap to add some life."
+            isPaused && generation == 0 && population > 0 -> "Ready to go."
+            isPaused && generation > 0 && population == 0 -> "Everyone died."
+            else -> "Paused"
+        }
     }
 }
